c2022b12b2c28ccbbc4ebaa1a59c5e633b53c891
Brenton Horne <brentonhorne77@gmail.com>
2016-12-12 07:48:53 +1000

    Adding comments into main.sh

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # atom-setup
 This repository contains the `main.sh` script which will, if run, build an AppImage for Atom on your local system, install a desktop configuration file for it and an icon.
+
+## One-Liner
+To execute `main.sh` in one line (regardless of whether you have a copy of this repo locally) merely run:
+
+```bash
+bash -c "$(wget -cqO- https://github.com/fusion809/atom-setup/raw/master/main.sh)"
+```
+
+## Dependencies
+In order to use this repo you only need the following programs to be installed:
+
+* cut
+* grep
+* SquashFS
+* sed
+* wget
--- main.sh
+++ main.sh
@@ -9,23 +9,50 @@ which wget >/dev/null 2>&1 || ( printf "wget missing" && exit 1 )
 which grep >/dev/null 2>&1 || ( printf "grep missing" && exit 1 )
 which sed >/dev/null 2>&1 || ( printf "sed missing" && exit 1 )
 which cut >/dev/null 2>&1 || ( printf "cut missing" && exit 1 )
+which mksquashfs >/dev/null 2>&1 || (printf "mksquashfs missing" && exit 1)
 
-APP=atom
-DLD=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f4)
-ATOM_VERSION=$(printf $DLD | cut -d '/' -f 8 | sed 's/v//g')
-if [[ -f "$HOME/.local/bin/$APP-$ATOM_VERSION*-$ARCH.AppImage" ]]; then
-  printf "The latest version of Atom is presently available on this system!" && exit 1
+printf "Welcome to the Atom AppImage builder and installer! If you experience a bug please report it at our bug tracker: https://github.com/fusion809/atom-setup/issues.\n"
+
+# Check architecture
+ARCH=$(uname -m)
+if ! [[ $ARCH == "x86_64" ]]; then
+  printf "Ah, buddy this script won't work on systems other than x86_64 and this script has detected you are running on a $ARCH system. So this script will exit. If this is an error on the part of this script please report it at https://github.com/fusion809/atom-setup/issues/.\n" && exit 1
 fi
 
-# Download to and work in /tmp
+# appname
+APP=atom
+
+# Download URL
+DLD=$(wget -q "https://api.github.com/repos/atom/atom/releases/latest"  -O - | grep -E "https.*atom-amd64.tar.gz" | cut -d '"' -f4)
+
+# Atom Version
+ATOM_VERSION=$(printf $DLD | cut -d '/' -f 8 | sed 's/v//g')
+printf "This script has determined the latest available stable release of Atom to be $ATOM_VERSION. If this is wrong please report this as this is a bug!\n"
+
+if [[ -f "$HOME/.local/bin/$APP-$ATOM_VERSION.glibc2.14-$ARCH.AppImage" ]]; then
+  printf "The latest version of Atom is presently available on this system, so this script will exit.\n" && exit 1
+fi
+
+# Work in /tmp
 cd /tmp
+# Download tarball
 wget -c $DLD
+# Extract its contents
 tar zxf atom*tar.gz
 
+# Make AppDir and its binary subdirectory
 mkdir -p AppDir/usr/bin
+
+# Copy Atom tarball contents across
 cp -r atom-*/* AppDir/usr/bin
+
+# Enter the AppDir directory
 cd AppDir
+
+# Move icon to top-level directory
 find . -name atom.png -exec cp {} atom.png \;
+
+# Create destop config file
 cat > atom.desktop <<EOF
 [Desktop Entry]
 Type=Application
@@ -34,9 +61,58 @@ Icon=atom
 Exec=atom %u
 Categories=Development;TextEditor;GTK;
 MimeType=application/javascript;application/json;application/x-desktop;application/x-httpd-eruby;application/x-httpd-php;application/x-httpd-php3;application/x-httpd-php4;application/x-httpd-php5;application/x-ruby;application/x-bash;application/x-csh;application/x-sh;application/x-zsh;application/x-shellscript;application/x-sql;application/x-tcl;application/xhtml+xml;application/xml;application/xml-dtd;application/xslt+xml;text/coffeescript;text/css;text/html;text/plain;text/xml;text/xml-dtd;text/x-bash;text/x-c++;text/x-c++hdr;text/x-c++src;text/x-c;text/x-chdr;text/x-csh;text/x-csrc;text/x-dsrc;text/x-diff;text/x-go;text/x-java;text/x-java-source;text/x-makefile;text/x-markdown;text/x-objc;text/x-perl;text/x-php;text/x-python;text/x-ruby;text/x-sh;text/x-zsh;text/yaml;inode/directory;
-Comment=The hackable text editor for the 21st century.
+Comment=The hackable text editor for the 21st Century.
 EOF
 
+# Add a symlink so that the atom and apm executables are available from the same folder
+( cd usr/bin && ln -sf resources/app/apm/bin/apm apm )
+
+# Download AppRun
+curl -OL "https://github.com/probonopd/AppImageKit/releases/download/5/AppRun" # (64-bit)
+chmod a+x ./AppRun
+
+# Strip unnecessary binaries, which reduces the size of the AppImage by 2 MB.
+find . -type f -executable -exec strip {} \;
+
+# Determine the minimum glibc version needed
+GLIBC_NEEDED=$(find . -type f -executable -exec strings {} \; | grep ^GLIBC_2 | sed s/GLIBC_//g | sort --version-sort | uniq | tail -n 1)
+
+# Generate version string used in the AppImage's filename
+VERSION=${ATOM_VERSION}.glibc$GLIBC_NEEDED
+
+# Print version string
+printf "VERSION is $VERSION\n"
+
+# cd out of AppDir
+cd ..
+
+# create out directory
+mkdir -p out
+
+# Delete any existing AppImages
+rm -f out/*.AppImage || true
+
+# Download AppImageAssistant
+curl -sL "https://github.com/probonopd/AppImageKit/releases/download/6/AppImageAssistant_6-x86_64.AppImage" > AppImageAssistant
+
+# Fix perms
+chmod a+x AppImageAssistant
+
+# Build AppImage
+./AppImageAssistant ./AppDir/ out/$APP"-"$VERSION"-"$ARCH".AppImage" || printf "Building the AppImage failed. Please check your SquashFS setup."
+
+# Set perms on AppImage
+chmod +x out/$APP"-"$VERSION"-"$ARCH".AppImage"
+
+# Move AppImage to $HOME/.local/bin
+printf "Moving AppImage to $HOME/.local/bin\n"
+if ! [[ -d $HOME/.local/bin ]]; then
+  mkdir -p $HOME/.local/bin
+fi
+cp out/*AppImage $HOME/.local/bin
+
+# Install desktop config file and icon
+printf "Installing desktop configuration file and icon to $HOME/.local/share"
 if ! [[ -d $HOME/.local/share/applications ]]; then
   mkdir -p $HOME/.local/share/applications
 fi
@@ -45,38 +121,8 @@ if ! [[ -d $HOME/.local/share/icons ]]; then
 fi
 cp atom.desktop $HOME/.local/share/applications/appimage-atom.desktop
 cp atom.png $HOME/.local/share/icons
-( cd usr/bin && ln -sf resources/app/apm/bin/apm apm )
-curl -OL "https://github.com/probonopd/AppImageKit/releases/download/5/AppRun" # (64-bit)
-chmod a+x ./AppRun
-find . -type f -executable -exec strip {} \;
-
-GLIBC_NEEDED=$(find . -type f -executable -exec strings {} \; | grep ^GLIBC_2 | sed s/GLIBC_//g | sort --version-sort | uniq | tail -n 1)
-VERSION=${ATOM_VERSION}.glibc$GLIBC_NEEDED
-printf "VERSION is $VERSION\n"
-
-cd ..
-
-ARCH=$(uname -m)
-if [[ "$ARCH" = "x86_64" ]] ; then
-	APPIMAGE=$APP"-"$VERSION"-x86_64.AppImage"
-fi
-if [[ "$ARCH" = "i686" ]] ; then
-	APPIMAGE=$APP"-"$VERSION"-i386.AppImage"
-fi
-
-mkdir -p out
-
-rm -f out/*.AppImage || true
-
-curl -sL "https://github.com/probonopd/AppImageKit/releases/download/6/AppImageAssistant_6-x86_64.AppImage" > AppImageAssistant
-chmod a+x AppImageAssistant
-./AppImageAssistant ./AppDir/ out/$APP"-"$VERSION"-"$ARCH".AppImage"
-chmod +x out/$APP"-"$VERSION"-"$ARCH".AppImage"
-if ! [[ -d $HOME/.local/bin ]]; then
-  mkdir -p $HOME/.local/bin
-fi
-cp out/*AppImage $HOME/.local/bin
-
 sed -i -e "s|Exec=atom|Exec=$HOME/.local/bin/$APP-$VERSION-$ARCH.AppImage|g" $HOME/.local/share/applications/appimage-atom.desktop
 chmod +x $HOME/.local/share/applications/appimage-atom.desktop
+
+# Remove AppDir
 rm -rf /tmp/AppDir
